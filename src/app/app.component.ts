5c898831560bf25edb9806c955e425bd4ab52693
Javiera Ignacia Izquierdo Martino <jizquierdo.3it@bancochile.cl>
2021-09-30 17:43:46 -0300

    hago un eventBiding para recibir el click sobre el elemento p y llamar al metodo onNameClick q esta en el ts del componente usando ÃlÃa propiedad sayHello para emitir un evento de tipo string, en el componente padre uso un event biding pasando el contenido al metodo onSayHello con el $  para recibir el mensaje. en el ts padre, recibo el mensaje onSayHello y lo muestro por consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public myName:string = "Javiera"
+  public myName:string = "Javiera";
+
+  onSayHello(message){
+    console.log(message);
+  }
 }
