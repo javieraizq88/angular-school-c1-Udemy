a87e139d71efeba01bee65709a7f07ebdd15baf3
Javiera Ignacia Izquierdo Martino <jizquierdo.3it@bancochile.cl>
2021-09-30 17:22:24 -0300

    pasar un atributo desde fuera del componente por input

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: `custom-hello`,
@@ -8,8 +8,8 @@ import { Component } from "@angular/core";
     ]
 })
 export class HelloComponent {
-    public name:string;
+    @Input () public name:string;
     constructor(){
-        this.name = `Javiera`;
+  
     };
 }
