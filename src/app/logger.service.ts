63bae90e757220ba8c301fc953610d24cd13993c
Javiera Ignacia Izquierdo Martino <jizquierdo.3it@bancochile.cl>
2021-10-07 16:20:47 -0300

    inspector, bundles y tree-shaking

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,3 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    // para q se cree la instancia en el modulo principal
+    providedIn: "root"
+})
+
 export class LoggerService {
     log(msg:string) {console.log(msg);}
     error(msg:string) {console.error(msg);}
